f0fcad656eb679dc56f78fb68a7ac7af3070ee39
URSTRULYAMIT <pathakap364056@gmail.com>
2023-10-08 17:27:09 +0530

    helloworld

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # demo
 first repo apna
 author :amit pathak 
+one of the best way to earn money 
---
+++ first.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"git hub is a version control system"<<endl;
+    return 0;
+    
+}
